330accb4131a8c468ff6494d5e16936373a84209
Vivek Singh <vivekkumarhcc245@gmail.com>
2025-11-18 17:49:27 +0530

    Home page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 
+
 const App = () => {
   return (
     <div>
+
       <Routes >
-        <Route path='/' element={<Home/>} />
-        <Route path='/blog' element={<Blog/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/blog/:id' element={<Blog />} />
       </Routes>
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Navbar />
       <Header />
       <BlogList />
+      <Newsletter />
+      <Footer />
     </>
   )
 }
